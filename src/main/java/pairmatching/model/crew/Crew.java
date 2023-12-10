a62dc1d2848a589d5190afd039f0968371fb05e3
Bosub Kim <grapeflavoredapple@gmail.com>
2023-12-10 14:09:34 +0900

    feat: Crew 정의
    크루원을 관리하는 클래스 Crew를 정의함

---
+++ src/main/java/pairmatching/model/crew/Crew.java
@@ -0,0 +1,20 @@
+package pairmatching.model.crew;
+
+public class Crew {
+    private Course course;
+    private String name;
+
+    private Crew(Course course, String name) {
+        this.course = course;
+        this.name = name;
+    }
+
+    public static Crew of(Course course, String name) {
+        return new Crew(course, name);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
