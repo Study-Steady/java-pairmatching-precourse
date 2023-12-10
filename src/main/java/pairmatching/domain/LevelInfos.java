c87e2f7516aa8f0065f7a2d1b371eed4e80b9d98
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-12-10 15:49:10 +0900

    feat : 레벨별 미션에 대한 정보를 갖는 객체 추가

---
+++ src/main/java/pairmatching/domain/LevelInfos.java
@@ -0,0 +1,44 @@
+package pairmatching.domain;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class LevelInfos {
+    private Map<Level, List<Mission>> levelInfos;
+
+    private LevelInfos(Map<Level, List<Mission>> levelInfos) {
+        this.levelInfos = levelInfos;
+    }
+
+    public static LevelInfos getInstance() {
+        Map<Level, List<Mission>> levelInfos = new HashMap<>();
+        levelInfos.put(Level.LEVEL1, Arrays.asList(Mission.CAR_RACING, Mission.LOTTO, Mission.NUMBER_BASEBALL));
+        levelInfos.put(Level.LEVEL2, Arrays.asList(Mission.CART, Mission.PAYMENT, Mission.SUBWAY_MAP));
+        levelInfos.put(Level.LEVEL4, Arrays.asList(Mission.PERFORMANCE_IMPROVEMENTS, Mission.DEPLOYMENT));
+        return new LevelInfos(levelInfos);
+    }
+
+    public List<String> getMissionNamesOfLevel(Level level) {
+        List<Mission> missions = levelInfos.get(level);
+        if (missions == null) {
+            return Collections.singletonList("");
+        }
+        return missions.stream()
+                .map(Mission::getName)
+                .collect(Collectors.toList());
+    }
+
+    public boolean isLevelContains(String levelName, String missionName) {
+        Level level = Level.getByName(levelName);
+        Mission mission = Mission.getByName(missionName);
+        List<Mission> missions = levelInfos.get(level);
+        return missions != null && missions.contains(mission);
+    }
+}
+
